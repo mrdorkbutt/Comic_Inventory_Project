9166f7f3ecf839f5497ab451dd63bbf693c509a4
Mark Kenney <mark@kenneywww.com>
2014-05-22 23:19:41 -0400

    space fixed in link

--- detail/index.php
+++ detail/index.php
@@ -7,11 +7,8 @@
 	<ul>
 
 	<?php
-	// Connect to database server
-	mysql_connect("localhost", "root", "") or die (mysql_error ());
 
-	// Select database
-	mysql_select_db("cjenkins_1_comicinventory.db") or die (mysql_error ());
+	include "../database_config.php";
 
 	// SQL query
 	$strSQL = "SELECT * From series ORDER BY title";
@@ -26,7 +23,7 @@
 	  $strName = $row['title'];
 
 	   // Create a link to series.php with the id-value in the URL
-	   $strLink = "<a href = 'series.php?id = " . $row['id'] . "'>" . $strName . "</a>";
+	   $strLink = "<a href = 'series.php?id=" . $row['id'] . "'>" . $strName . "</a>";
 
 	    // List link
 	   echo "<li>" . $strLink . "</li>";
