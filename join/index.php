12e01682eaae908269e1a62cc3328afe3d79b525
Mark Kenney <mark@kenneywww.com>
2014-05-22 23:29:33 -0400

    use the database config for this as well

--- join/index.php
+++ join/index.php
@@ -7,11 +7,8 @@
 <body>
 <!-- is this workin g-->
 <?php
-// Connect to database server
-mysql_connect("localhost", "root", "") or die (mysql_error ());
 
-//select database
-mysql_select_db("cjenkins_1_comicinventory.db") or die (mysql_error ());
+  include "../database_config.php";
 
 //SQL query
 
