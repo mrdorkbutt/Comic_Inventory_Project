510116b9c3c6f5cd410e7190786b776deeebb2f2
Mark Kenney <mark@kenneywww.com>
2014-05-27 15:47:20 -0400

    index.php

---
+++ PDO/detail/index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+<title>Inventory with detail page in PDO</title>
+
+</head>
+
+<body>
+ 
+  <ul>
+
+<?php
+// Connect to database server
+require_once "../comicinventory_connect.php";
+$dbh = comicinventorydb_connect();
+
+
+//SQL query
+$sth = $dbh->query ("SELECT * From series ORDER BY title");
+
+
+// Execute the Query
+$sth->execute();
+
+// Loop the recordset $rs
+// Each row will be made into an array ($row) using mysql_fetch_array
+
+while ($rs = $sth->fetch(PDO::FETCH_OBJ)){
+	// Write the value of the column FirstName (which is now in the array $row)
+
+// this is the problem area - Corey
+
+	   // Create a link to series.php with the id-value in the URL
+	   $strLink = "<a href = 'series.php?id=".$rs->id. "'>" .#rs->title. "</a>";
+
+	    // List link
+	   echo "<li>" . $strLink . "</li>";
+
+	  }
+
+//echo "Title: ".$rs->title."<br>";
+//echo "Publisher: ".$rs->publisher."<br>";
+//}
+
+// end of problem area - Corey
+	
+// close the database connection
+$dbh = NULL;
+
+?>
+	</ul>
+
+
+</body>
+
+
+</html>
