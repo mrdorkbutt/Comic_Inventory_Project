f068b9e67d5b8f7c7c8e76ad1b18eef719c9c420
Corey <cjenkins@screamingkid.com>
2014-06-09 00:48:23 -0600

    Working with the login trying to setup the Hash
    Some issues, but most of it works.

--- login.php
+++ login.php
@@ -3,7 +3,41 @@
 if ($_POST ){
     $form_data = $_POST;
     print_r( $form_data );
-    //do login stuff
+    
+	$username = $_POST[ 'username' ];
+$password = $_POST[ 'password' ];
+var_dump($_POST);
+	require_once "comicinventory_connect.php";
+ 	$db = comicinventorydb_connect();
+
+$sql = "SELECT * FROM user WHERE username=:username";
+
+$query = $db->prepare( $sql );
+$query->execute( array( ':username'=>$username ) );
+$results = $query->fetchAll( PDO::FETCH_ASSOC ); 
+
+foreach( $results as $row ){ 
+    $password_hash = $row[ 'password' ];
+    print_r( $password_hash );
+}
+
+include ( 'PasswordHash.php' );
+
+$hash_obj = new PasswordHash( 8, false );
+$check = $hash_obj->CheckPassword( $password, $stored_hash );
+
+if ( $check ){
+   print_r( "This is a valid user" );
+} else {
+   print_r( "Authentication failed, please Try again.");
+}
+
+
+}	?>
+
+	
+	
+	//do login stuff
 } else { 
 	?>
 
@@ -17,5 +51,4 @@ if ($_POST ){
             </div>
             <button type="submit" class="btn btn-success">Sign in</button>
           </form>
-     
-<?php } ?>
+}
